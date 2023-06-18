5051803c582b9824ed46ab60aee47faeb8066b13
Uche Odinamba <uchay2x@icloud.com>
2023-06-18 15:19:54 +0200

    alpha beta pruning

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ function makeAIPlay() {
   for (let i = 0; i < 9; i++) {
     if (board[i] === "") {
       board[i] = "O";
-      let score = alphaBetaMinimax(board, 0, false);
+      let score = alphaBetaMinimax(board, 0, -Infinity, Infinity, false);
       board[i] = "";
       if (score > bestScore) {
         bestScore = score;
@@ -99,6 +99,7 @@ function toggle() {
   }
   resetGame();
 }
+
 // Function to evaluate the score of the current board state
 function evaluate(board) {
   for (let combination of winningCombinations) {
@@ -114,40 +115,49 @@ function evaluate(board) {
   return 0;
 }
 
-// Alpha-Beta search algorithm
-function alphaBetaMinimax(board, depth, isMaximizingPlayer) {
-    let score = evaluate(board); // Evaluate the current board state
-  
-    if (score === 1 || score === -1) {
-      return score; // Return the score if the game is already won or lost
-    }
-  
-    if (board.indexOf("") === -1) {
-      return 0; // Return 0 if the game is a draw
-    }
-  
-    if (isMaximizingPlayer) {
-      let bestScore = -Infinity;
-      for (let i = 0; i < 9; i++) {
-        if (board[i] === "") {
-          board[i] = "O"; // Make a move for the maximizing player (AI)
-          let score = alphaBetaMinimax(board, depth + 1, false); // Recursively call alphaBetaMinimax for the minimizing player (human)
-          board[i] = ""; // Undo the move
-          bestScore = Math.max(score, bestScore); // Update the best score
-        }
-      }
-      return bestScore; // Return the best score for the maximizing player
-    } else {
-      let bestScore = Infinity;
-      for (let i = 0; i < 9; i++) {
-        if (board[i] === "") {
-          board[i] = "X"; // Make a move for the minimizing player (human)
-          let score = alphaBetaMinimax(board, depth + 1, true); // Recursively call alphaBetaMinimax for the maximizing player (computer)
-          board[i] = ""; // Undo the move
-          bestScore = Math.min(score, bestScore); // Update the best score
-        }
-      }
-      return bestScore; // Return the best score for the minimizing player
-    }
+// Alpha-Beta search algorithm with pruning
+function alphaBetaMinimax(board, depth, alpha, beta, isMaximizingPlayer) {
+  let score = evaluate(board); // Evaluate the current board state
+
+  if (score === 1 || score === -1) {
+    return score; // Return the score if the game is already won or lost
   }
+
+  if (board.indexOf("") === -1) {
+    return 0; // Return 0 if the game is a draw
+  }
+
+  if (isMaximizingPlayer) {
+    let bestScore = -Infinity; // Initialize the best score as negative infinity for maximizing player
+    for (let i = 0; i < 9; i++) {
+      if (board[i] === "") {
+        board[i] = "O"; // Make a move for the maximizing player (AI)
+        let currentScore = alphaBetaMinimax(board, depth + 1, alpha, beta, false); // Recursively call alphaBetaMinimax for the minimizing player (human)
+        board[i] = ""; // Undo the move
+        bestScore = Math.max(bestScore, currentScore); // Update the best score
+        alpha = Math.max(alpha, bestScore); // Update alpha with the best score
+        if (alpha >= beta) {
+          break; // Alpha-beta pruning: if alpha is greater than or equal to beta, no need to explore further
+        }
+      }
+    }
+    return bestScore; // Return the best score for the maximizing player
+  } else { // isMinimizingPlayer
+    let bestScore = Infinity; // Initialize the best score as positive infinity for minimizing player
+    for (let i = 0; i < 9; i++) {
+      if (board[i] === "") {
+        board[i] = "X"; // Make a move for the minimizing player (human)
+        let currentScore = alphaBetaMinimax(board, depth + 1, alpha, beta, true); // Recursively call alphaBetaMinimax for the maximizing player (AI)
+        board[i] = ""; // Undo the move
+        bestScore = Math.min(bestScore, currentScore); // Update the best score
+        beta = Math.min(beta, bestScore); // Update beta with the best score
+        if (alpha >= beta) {
+          break; // Alpha-beta pruning: if alpha is greater than or equal to beta, no need to explore further
+        }
+      }
+    }
+    return bestScore; // Return the best score for the minimizing player
+  }
+}
+
   
